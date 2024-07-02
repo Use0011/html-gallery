3854654060e1448d279bf2106b85d07b5c162c23
Use0011 <gibuypicture@gmail.com>
2024-07-02 13:36:37 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('.pages a').
-forEach(link => {
-    if(link.href.includes(`${activePage}`)){
-        link.classList.add('active');
-    }
-})
